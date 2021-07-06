509f99965d804b6c76aaf0a3d14be8dd87448aae
Peter Nahm <hwnahm@gmail.com>
2021-07-06 19:45:18 +0900

    fix: overcome the TAL price difference

--- src/exchange/pricing.ts
+++ src/exchange/pricing.ts
@@ -3,9 +3,11 @@ import { BigDecimal, Address } from "@graphprotocol/graph-ts/index";
 import { Pair, Token, Bundle } from "../../generated/schema";
 import { ZERO_BD, factoryContract, ADDRESS_ZERO, ONE_BD } from "./utils";
 
+// TODO: Farms & Pools are using only USDC pair for calculating ETH price
 let WETH_ADDRESS = "0x46884d7849223e057226a69e5f8215d6ff1b8bd6";
 let USDC_WETH_PAIR = "0x65f1eb2bcb4b1d9a043366de732f0f7e055d2fab"; // created block 10566560
-let USDT_WETH_PAIR = "0xaddb08d7f1c5c29243f85e7b400be6aacd3298f2"; // created block 10539109
+// let USDT_WETH_PAIR = "0xaddb08d7f1c5c29243f85e7b400be6aacd3298f2"; // created block 10539109
+let USDT_WETH_PAIR = ""; // created block 10539109
 
 export function getEthPriceInUSD(): BigDecimal {
   // fetch eth prices for each stablecoin
