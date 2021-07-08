d9be20d6c1d9817d84624fc149a55023cb806695
Peter Nahm <hwnahm@gmail.com>
2021-07-08 10:51:31 +0900

    fix: weth coin address

--- src/exchange/pricing.ts
+++ src/exchange/pricing.ts
@@ -34,7 +34,7 @@ export function getEthPriceInUSD(): BigDecimal {
 
 // token where amounts should contribute to tracked volume and liquidity
 let WHITELIST: string[] = [
-  "0x46884d7849223e057226a69e5f8215d6ff1b8bd6", // WETH
+  "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2", // WETH
   "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48", // USDC
   "0xdac17f958d2ee523a2206206994597c13d831ec7", // USDT
   // "0x8ac76a51cc950d9822d68b83fe1ad97b32cd580d", // USDC
